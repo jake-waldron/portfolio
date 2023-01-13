158a1eb8a031881fe8371aa423eba8778137d8b7
Jake Waldron <jakewaldron@gmail.com>
2023-01-12 20:00:45 -0500

    simple styles

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,19 +4,29 @@ import styled from 'styled-components';
 
 export default function Header() {
 	return (
-		<StyledHeader>
-			<h2>Jake Waldron</h2>
+		<header>
 			<StyledNav>
-				<Link href="/">Home</Link>
-				<Link href="/about">About</Link>
-				<Link href="/projects">Projects</Link>
-				<Link href="/contact">Contact</Link>
+				<StyledH2>Jake Waldron</StyledH2>
+				<StyledUl>
+					<li>
+						<Link href="/">Home</Link>
+					</li>
+					<li>
+						<Link href="/about">About</Link>
+					</li>
+					<li>
+						<Link href="/projects">Projects</Link>
+					</li>
+					<li>
+						<Link href="/contact">Contact</Link>
+					</li>
+				</StyledUl>
 			</StyledNav>
-		</StyledHeader>
+		</header>
 	);
 }
 
-const StyledHeader = styled.header`
+const StyledNav = styled.nav`
 	background: #000;
 	color: #fff;
 	padding: 1rem;
@@ -25,15 +35,30 @@ const StyledHeader = styled.header`
 	align-items: center;
 `;
 
-const StyledNav = styled.nav`
+const StyledH2 = styled.h2`
+	color: #fff;
+	margin-left: 100px;
+`;
+
+const StyledUl = styled.ul`
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
-	width: 25%;
+	min-width: 350px;
 	margin-right: 100px;
+
+	li {
+		list-style: none;
+	}
 
 	a {
 		color: #fff;
 		text-decoration: none;
+	}
+
+	a:hover {
+		/* text-decoration: underline; */
+		padding-bottom: 4px;
+		border-bottom: 4px solid #fff;
 	}
 `;
