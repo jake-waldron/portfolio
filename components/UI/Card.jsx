2f6cdd40d652048683f97598cf4675308d557632
Jake Waldron <jakewaldron@gmail.com>
2023-01-15 07:14:36 -0500

    update card styling

--- components/UI/Card.jsx
+++ components/UI/Card.jsx
@@ -2,11 +2,14 @@ import Link from 'next/link';
 import React from 'react';
 import { motion } from 'framer-motion';
 import styled from 'styled-components';
+import { useRouter } from 'next/router';
 
 export default function Card(props) {
+	const router = useRouter();
 	function handleClick() {
 		// navigate to props.url
 		console.log('go to project page!');
+		router.push(`/projects/${props.slug}`);
 	}
 
 	return (
@@ -30,6 +33,16 @@ const StyledCard = styled(motion.button)`
 	justify-content: center;
 	align-items: center;
 	flex: 1 1 0px;
+
+	img {
+		/* width: 100%; */
+		/* height: 100%; */
+		/* object-fit: cover; */
+		border-radius: 16px;
+		margin: 16px 0;
+		/* box-shadow: 0 0 28px rgba(0, 0, 0, 0.2); */
+		border: 1px solid #000;
+	}
 
 	p {
 		justify-self: center;
